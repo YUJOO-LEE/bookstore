9205e62be026914812083d5ec611723d03caf363
YUJOO-LEE <lllllllllee@gmail.com>
2022-10-24 01:10:34 +0900

    스크롤 좌표에 대한 이벤트 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,30 +3,53 @@ import { faBars, faMagnifyingGlass, faX, faPenNib, faAt } from '@fortawesome/fre
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { NavLink, Link } from 'react-router-dom';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Header(props) {
 
+  // 헤더
+  const Header = useRef(null);
+  const [ scrollPosition, setScrollPosition ] = useState(1);
+  const [ isHeaderOn, setHeaderOn ] = useState(true);
+  const [ isScrollTop, setScrollTop ] = useState(true);
+
+  // 모바일 메뉴
   const mobileNav = useRef(null);
   const [ isMobileNavOn, setMobileNav ] = useState(false);
 
-  function toggleMobileNav(e) {
+  // 모바일 메뉴 노출
+  const toggleMobileNav = (e)=>{
     e.preventDefault();
     !isMobileNavOn ? setMobileNav(true) : setMobileNav(false);
   }
 
+  // 헤더 스크롤 이벤트
+  const handleScroll = ()=>{
+    const moving = window.scrollY; 
+    setScrollTop(moving < 100);
+    setHeaderOn(scrollPosition > moving);
+    setScrollPosition(moving);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScroll)
+    return (()=>{
+      window.removeEventListener('scroll', handleScroll)
+    });
+  }, [scrollPosition]);
+
   return (
-    <header id='header' className={props.type}>
+    <header id='header' className={`${props.type} ${isHeaderOn && 'on'} ${isScrollTop && 'top'}`} ref={Header}>
       <div className='subNav'>
         <div className='inner'>
           <ul>
             <li>
-              <NavLink to={'/about'} activeClassName='on'>
+              <NavLink to='/about' activeClassName='on'>
               About
               </NavLink>
             </li>
             <li>
-              <NavLink to={'/store'} activeClassName='on'>
+              <NavLink to='/store' activeClassName='on'>
               Store
               </NavLink>
             </li>
@@ -36,11 +59,11 @@ export default function Header(props) {
       <div className='mainNav'>
         <div className='inner'>
           <nav id='gnb'>
-            <Link className='btnOpenMobileNav' onClick={toggleMobileNav}>
+            <Link to='#' className='btnOpenMobileNav' onClick={toggleMobileNav}>
               <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
             </Link>
             <h1>
-              <NavLink to={'/'} activeClassName='on'>
+              <NavLink to='/' activeClassName='on'>
               <p>Yu</p>
               <p><span></span><span></span></p>
               <p>joo</p>
@@ -48,22 +71,22 @@ export default function Header(props) {
             </h1>
             <ul>
               <li>
-                <NavLink to={'/book'} activeClassName='on'>
+                <NavLink to='/book' activeClassName='on'>
                 Book
                 </NavLink>
               </li>
               <li>
-                <NavLink to={'/review'} activeClassName='on'>
+                <NavLink to='/review' activeClassName='on'>
                 Review
                 </NavLink>
               </li>
               <li>
-                <NavLink to={'/photo'} activeClassName='on'>
+                <NavLink to='/photo' activeClassName='on'>
                 Photo
                 </NavLink>
               </li>
               <li>
-                <NavLink to={'/video'} activeClassName='on'>
+                <NavLink to='/video' activeClassName='on'>
                 Video
                 </NavLink>
               </li>
@@ -75,7 +98,7 @@ export default function Header(props) {
 
           <div className="navBtns">
             <span className='login'>
-              <NavLink to={'/login'} activeClassName='on'>
+              <NavLink to='/login' activeClassName='on'>
               Login / Join
               </NavLink>
             </span>
@@ -88,12 +111,12 @@ export default function Header(props) {
 
       <div className={`mobileNav ${isMobileNavOn && 'on'}`} ref={mobileNav}>
         <h1>
-          <NavLink to={'/'} activeClassName='on' className='logo'>
+          <NavLink to='/' activeClassName='on' className='logo'>
             <p>Yu</p>
             <p><span></span><span></span></p>
             <p>joo</p>
           </NavLink>
-          <Link className='btnCloseMobileNav' onClick={toggleMobileNav}>
+          <Link to='#' className='btnCloseMobileNav' onClick={toggleMobileNav}>
             <FontAwesomeIcon icon={faX}></FontAwesomeIcon>
           </Link>
         </h1>
@@ -105,22 +128,22 @@ export default function Header(props) {
 
         <ul className='mobileMainNav'>
           <li>
-            <NavLink to={'/book'} activeClassName='on'>
+            <NavLink to='/book' activeClassName='on'>
             Book
             </NavLink>
           </li>
           <li>
-            <NavLink to={'/review'} activeClassName='on'>
+            <NavLink to='/review' activeClassName='on'>
             Review
             </NavLink>
           </li>
           <li>
-            <NavLink to={'/photo'} activeClassName='on'>
+            <NavLink to='/photo' activeClassName='on'>
             Photo
             </NavLink>
           </li>
           <li>
-            <NavLink to={'/video'} activeClassName='on'>
+            <NavLink to='/video' activeClassName='on'>
             Video
             </NavLink>
           </li>
@@ -128,12 +151,12 @@ export default function Header(props) {
 
         <ul className='mobileSubNav'>
           <li>
-            <NavLink to={'/about'} activeClassName='on'>
+            <NavLink to='/about' activeClassName='on'>
             About
             </NavLink>
           </li>
           <li>
-            <NavLink to={'/store'} activeClassName='on'>
+            <NavLink to='/store' activeClassName='on'>
             Store
             </NavLink>
           </li>
