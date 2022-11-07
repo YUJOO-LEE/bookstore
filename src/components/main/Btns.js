8a5fa0520352b75c6306b363707477a0ff58f8bf
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-07 16:05:31 +0900

    btns 디자인 확정 (책갈피모양)

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,51 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPanorama, faBook, faFeather, faCameraRetro, faFilm } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
 
 export default function Btns(props) {
-  const [ Change, setChange ] = useState(true);
 
   return (
-    <>
-      <button
-        style={{position: 'absolute', top: 0, right: 0, zIndex: 9999}}
-        onClick={()=>setChange(!Change)}
+    <ul className='scrollNavi'>
+      <li
+        onClick={()=>props.setIndex(0)}
       >
-        <span>Change</span>
-      </button>
-
-      <ul className={`scrollNavi ${Change ? 'one': 'two'}`}>
-        <li
-          onClick={()=>props.setIndex(0)}
-        >
-          <FontAwesomeIcon icon={faPanorama}></FontAwesomeIcon>
-          <span>Visual</span>
-        </li>
-        <li
-          onClick={()=>props.setIndex(1)}
-        >
-          <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
-          <span>Books</span>
-        </li>
-        <li
-          onClick={()=>props.setIndex(2)}
-        >
-          <FontAwesomeIcon icon={faFeather}></FontAwesomeIcon>
-          <span>Reviews</span>
-        </li>
-        <li
-          onClick={()=>props.setIndex(3)}
-        >
-          <FontAwesomeIcon icon={faCameraRetro}></FontAwesomeIcon>
-          <span>Photos</span>
-        </li>
-        <li
-          onClick={()=>props.setIndex(4)}
-        >
-          <FontAwesomeIcon icon={faFilm}></FontAwesomeIcon>
-          <span>Videos</span>
-        </li>
-      </ul>
-    </>
+        <FontAwesomeIcon icon={faPanorama}></FontAwesomeIcon>
+        <span>Visual</span>
+      </li>
+      <li
+        onClick={()=>props.setIndex(1)}
+      >
+        <FontAwesomeIcon icon={faBook}></FontAwesomeIcon>
+        <span>Books</span>
+      </li>
+      <li
+        onClick={()=>props.setIndex(2)}
+      >
+        <FontAwesomeIcon icon={faFeather}></FontAwesomeIcon>
+        <span>Reviews</span>
+      </li>
+      <li
+        onClick={()=>props.setIndex(3)}
+      >
+        <FontAwesomeIcon icon={faCameraRetro}></FontAwesomeIcon>
+        <span>Photos</span>
+      </li>
+      <li
+        onClick={()=>props.setIndex(4)}
+      >
+        <FontAwesomeIcon icon={faFilm}></FontAwesomeIcon>
+        <span>Videos</span>
+      </li>
+    </ul>
   );
 }
