49c9b6be8ef607e574b6e3643fa39b8c037b1cab
YUJOO-LEE <lllllllllee@gmail.com>
2022-11-08 22:18:18 +0900

    추후에 알아보기 쉽게 주석 추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -12,31 +12,34 @@ export default function Visual() {
   const [ Timer, setTimer ] = useState(false);
   const [ TimerBtn, setTimerBtn ] = useState(true);
 
+  // 타이머 작동여부에 따라 인덱스 자동 변경
   useEffect(()=>{
     if (!Timer) return;
-    setTimerBtn(true);
+    setTimerBtn(true);  // 타이머 작동 시 재생버튼 활성화
 
     const showNext = setTimeout(()=>{
-      setTimer(false);
+      setTimer(false);  // class 순간적으로 빼기 위해 타이머 일시정지
       if (Index < 4) {
-        setIndex(Index + 1);
+        setIndex(Index + 1);  // 인덱스 마지막 번호보다 낮으면 +1
       } else {
-        setIndex(0);
+        setIndex(0);  // 인덱스 마지막번호라면 0으로 초기화
       };
       setTimeout(()=>{
-        setTimer(true);
+        setTimer(true); // class 다시 부여하기 위해 타이머 재생
       })
     }, 5000);
 
-    return (()=>{
+    return (()=>{ // 업데이트 시 타이머 클리어
       clearTimeout(showNext);
     });
   }, [Timer, Index]);
 
+  // 타이머 재생버튼 클릭에 실제 타이머 연동 
   useEffect(()=>{
     setTimer(TimerBtn);
   }, [TimerBtn])
 
+  // 마운트 시 자동 재생
   useEffect(()=>{
     setTimer(true);
   }, [])
